6248a8d0c924763f6945dd3cb918d76cde110036
shaon-hossain45 <shaonhossain915@gmail.com>
2022-03-22 19:47:20 +0600

    updated something

---
+++ admin/partials/lib/headline.php
@@ -0,0 +1,10 @@
+<div class="wf-row">
+	<div class="wf-col-xs-12">
+		<div class="wp-header-end"></div>
+		<div class="wf-section-title wf-add-top">
+		    <div class="wordfence-headline-icon wf-hidden-xs"><span class="dashicons dashicons-excerpt-view"></span></div>
+	        <h3 class="wf-center-xs">Public Popularity</h3>
+		    <span class="wf-hidden-xs"><a href="#" target="_blank" rel="noopener noreferrer" class="wf-help-link">Learn more about the Dashboard<span class="screen-reader-text"> (<?php esc_html_e( 'opens in new tab', 'wordfence' ); ?>)</span></a> <i class="wf-fa wf-fa-external-link" aria-hidden="true"></i></span>
+        </div>
+	</div>
+</div>
--- admin/partials/views/page.php
+++ admin/partials/views/page.php
@@ -1,2 +1,10 @@
-<?php
-echo "page start";
+<div class="wrap wordfence" id="wf-dashboard">
+	<div class="wf-container-fluid">
+<!-- headline start -->
+<?php include(plugin_dir_path( __FILE__ ) . '../lib/headline.php'); ?>
+<!-- headline end -->
+<!-- begin notifications -->
+<?php include(plugin_dir_path( __FILE__ ) . '../lib/notification.php'); ?>
+<!-- end notifications -->
+    </div>
+</div>
